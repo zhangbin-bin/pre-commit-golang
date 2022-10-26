bec698ee8cf1923a7a8b0a98647c06a031be2350
zhangbin.bin <zhangbin.bin@bytedance.com>
2022-10-26 11:19:51 +0800

    gofumpt

--- run-go-fmt.sh
+++ run-go-fmt.sh
@@ -4,11 +4,11 @@
 #
 set -e -o pipefail
 
-if ! command -v gofmt &> /dev/null ; then
+if ! command -v gofumpt &> /dev/null ; then
     echo "gofmt not installed or available in the PATH" >&2
     exit 1
 fi
 
 exec 5>&1
-output="$(gofmt -l -w "$@" | tee /dev/fd/5)"
+output="$(gofumpt -l -w "$@" | tee /dev/fd/5)"
 [[ -z "$output" ]]
